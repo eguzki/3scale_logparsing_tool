841cd41bc1267b2ca2d7fe02cf13762b315df9db
Eguzki Astiz Lezaun <eastizle@redhat.com>
2018-02-21 16:47:18 +0100

    add progress bar

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'parallel', '1.12.0'
+gem 'ruby-progressbar', '1.9.0'
--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ ARGV.size == 1 || abort('error: no path specified')
 File.directory?(ARGV[0]) || abort('error: path not found')
 
 logfile_array = Dir.entries(ARGV[0]).select { |f| !File.directory? f }.map { |f| File.join(ARGV[0], f) }
-logstats = Parallel.map(logfile_array) { |f| parse_logfile f }
+logstats = Parallel.map(logfile_array, progress: "Parsing log files") { |f| parse_logfile f }
 res = reduce_logstats logstats
 ui res
 exit 0
